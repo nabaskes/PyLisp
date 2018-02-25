54551b021d792fe39d519b5e8bfc1dfaaf0d6617
nabaskes <nabaskes@uchicago.edu>
2018-02-24 20:58:51 -0500

    fixed tree constructor, started adding functions

--- interpreter.py
+++ interpreter.py
@@ -1,8 +1,41 @@
+from tree import SyntaxTree
+
 class Interpreter:
     def __init__(self):
         self.builtin = ['+', '-', '/', '*']
-        self.functions = ['eq?', 'quote', 'cons', 'car', 'cdr', 'atom?', 'define', 'lambda', 'cond']
+        self.base_functions = ['eq?', 'quote', 'cons', 'car', 'cdr', 'atom?', 'define', 'lambda', 'cond']
+        self.defined = {}
+        self.fake_tree = SyntaxTree('(+ 1 2)', self)
 
     def __call__(self, expr):
         'Evaluates a lisp expression'
-        pass
+        tree = SyntaxTree(expr)
+        return tree()
+
+    def evalbuiltin(self, symbol, left, right):
+        if symbol == '+':
+            return left + right
+        elif symbol == '-':
+            return left - right
+        elif symbol == '/':
+            return left/right
+        elif symbol == '*':
+            return left*right
+
+    def evalbase(self, symbol, left, right):
+        if symbol == 'eq?':
+            return self.equals(left, right)
+        if symbol == ''
+
+    def equals(self, left, right):
+        if self.is_tree(left):
+            if self.is_tree(right):
+                return self.equals(left.left, right.left) and self.equals(left.right, right.right)
+            return False
+        if self.is_tree(right):
+            return False
+        return left == right
+
+    def is_tree(self, item):
+        'tests whether an item is a tree'
+        return type(item) == type(self.fake_tree)
--- tree.py
+++ tree.py
@@ -2,22 +2,57 @@ class SyntaxTree:
     def __init__(self, expr, interpreter):
         self.raw_expr = expr
         self.interpreter = interpreter
+        self.naive_tree = False
         self.make_tree()
 
+    def __call__(self):
+        try:
+            self.left = self.left()
+        except Exception:
+            # left is not a SyntaxTree
+            pass
+        try:
+            self.right = self.right()
+        except Exception:
+            pass
+        return self.eval()
+
     def make_tree(self):
-        naive_split = self.raw_expr.split(" ")
-        self.symbol = naive_split[0].replace("(", "")
-        if(len(naive_split) == 3):
-            self.val1 = naive_split[1]
-            self.val2 = naive_split[2]
+        self.first_sp = self.raw_expr.index(' ')
+        self.symbol = self.raw_expr[1:self.first_sp]
+        if(len(self.raw_expr.split(" ")) == 3):
             self.naive_tree = True
+            self.left = self.raw_expr.split(" ")[1]
+            self.right = self.raw_expr.split(" ")[2]
             return
-        elif(len(naive_split) < 3):
-            raise ValueError("Cannot have a lisp expression without 3 items")
-        elif("(" not in naive_split[1]):
-            self.val1 = naive_split[1]
-            self.val2 = SyntaxTree(self.raw_expr[self.raw_expr[self.raw_expr.index(' ')+1:].index(' ')+1:-1])
-            self.naive_tree = False
-            return
-        else:
-            expr1startin = self.raw_expr(" ")+1
+        self.second_sp = self.first_sp + self.group_parens(self.raw_expr[self.first_sp])+1
+        if self.second_sp-self.first_sp == 1:
+            # the second item is not a list
+            self.second_sp = self.raw_expr[self.first_sp+1:].index(' ')
+        self.left = SyntaxTree(self.raw_expr[self.first_sp+1:self.second_sp],
+                               self.interpreter)
+        self.right = SyntaxTree(self.raw_expr[self.second_sp+1:-1],
+                                self.interpreter)
+
+    def group_parens(self, string):
+        'returns the index of the parenthesis that corresponds to the first one in the string given'
+        opens = 0
+        closes = 0
+        for i in range(len(string)):
+            if string[i] == "(":
+                opens += 1
+            elif string[i] == ")":
+                closes += 1
+            if opens == closes:
+                return i
+        raise(ValueError("Open Paren doesn't have a corresponding close paren"))
+
+    def eval(self):
+        if self.symbol in self.interpreter.builtin:
+            return self.interpreter.evalbuiltin(self.symbol,
+                                                self.left,
+                                                self.right)
+        elif self.symbol in self.interpreter.base_functions:
+            return self.interpreter.evalbase(self.symbol,
+                                             self.left,
+                                             self.right)
